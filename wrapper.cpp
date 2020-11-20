37c97023b43e9b71b329b92d2a13295ee1fc9bd0
Rio Yokota <rioyokota@gsic.titech.ac.jp>
2020-11-20 22:36:19 +0900

    Loop N

--- wrapper.cpp
+++ wrapper.cpp
@@ -5,8 +5,8 @@ extern "C" double m_hacapk_calc_entry_ij_mp_hacapk_entry_ij_(int&, int&);
 
 extern "C" void hicma_(int& N) {
   std::vector<std::vector<double>> k1(N, std::vector<double>(N));
-  for(int i=0; i<10; i++) {
-    for(int j=0; j<10; j++) {
+  for(int i=0; i<N; i++) {
+    for(int j=0; j<N; j++) {
       int I = i+1, J = j+1;
       k1[i][j] = m_hacapk_calc_entry_ij_mp_hacapk_entry_ij_(I, J);
       printf("%6.2e ",k1[i][j]);
